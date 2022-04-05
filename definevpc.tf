88565fde82b3de77b101ef08cc5b01bfba2c00fb
vishnukr3 <78414950+vishnukr3@users.noreply.github.com>
2022-04-05 15:07:13 +0200

    Update definevpc.tf

--- definevpc.tf
+++ definevpc.tf
@@ -1,3 +1,41 @@
+#Define Variables for VPC
+
+variable "aws_region" {
+    description = "region to deploy EC2"
+    default = "us-east-1"
+}
+
+variable "aws_ami" {
+    description = "ami to use ec2 to launch"
+    default = "ami-04505e74c0741db8d"
+}
+
+variable "key_path" {
+    description = "key path used to fetch the private key"
+    default = "Universal_key"
+}
+
+variable "key_name" {
+    description = "the name of the key"
+    default = "Universal_key"
+}
+
+variable "vpc_cidr" {
+    description = "cidr notation for our custom vpc"
+    default = "10.0.0.0/16"  
+}
+
+variable "public_subnet_cidr" {
+    description = "cidr notation for our public subnet"
+    default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+    description = "cidr notation for our private subnet"
+    default = "10.0.2.0/24"
+}
+
+
 #define custom vpc
 
 resource "aws_vpc" "default" {
